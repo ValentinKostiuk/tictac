33721ba185f983dbf1ce3ce67a9bac19ee783b06
Valentin <Valentin>
2017-02-26 21:29:12 +0200

    make move and get field state implementation

--- serverApp/classes/GameField.ts
+++ serverApp/classes/GameField.ts
@@ -1,10 +1,12 @@
 import {Cell} from './Cell'
+import {CellStates} from './CellStates'
 
 export class GameField {
 	public cells: Cell[][];
 	private scores: number[];
+	private fieldSize: number;
 
-	constructor(fieldSize: number = 3) {
+	public constructor(fieldSize: number = 3) {
 
 		for (let i = 0; i < fieldSize; i++) {
 			for (let j = 0; j < fieldSize; j++) {
@@ -12,6 +14,34 @@ export class GameField {
 			}
 		}
 
-		this.scores = Array.apply(null, Array(fieldSize + 2)).map(Number.prototype.valueOf, 0);
+		this.fieldSize = fieldSize;
+		this.scores = Array.apply(null, Array(2 * fieldSize + 2)).map(Number.prototype.valueOf, 0);
+	}
+
+	public makeMove(x: number, y: number, playerMark: CellStates): void {
+		let point = playerMark === CellStates.cross ? 1 : -1;
+		this.cells[x][y].state = playerMark;
+
+		this.scores[x] += point;
+
+		this.scores[this.fieldSize + y] += point;
+
+		if (x == y) {
+			this.scores[2 * this.fieldSize] += point;
+		}
+
+		if (this.fieldSize - 1 - y == x) {
+			this.scores[2 * this.fieldSize + 1] += point;
+		}
+	}
+
+	public getFieldState (): CellStates {
+		let length = 2 * this.fieldSize + 2;
+		for (let i = 0; i < length; i += 1) {
+			if (Math.abs(this.scores[i]) === this.fieldSize) {
+				return this.scores[i] > 0 ? CellStates.cross : CellStates.nought;
+			}
+		}
+		return CellStates.empty;
 	}
 }
